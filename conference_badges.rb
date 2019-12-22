ee760aea50f791c6a9276b85ea4f8885efed0b3e
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 00:29:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+attendees = ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+batch_badge_creator(attendees)
+batch_badge_creator.each do |attendee|
+  puts "Hello, my name is #{attendee}"
+end
+
+assign_rooms = ["1", "2", "3" "4", "5", "6", "7"]
+assign_rooms(attendees)
+assign_rooms.each do |attendee|
+  puts "Hello, #{attendee}! You'll be assigned to room #{assign_room}"
+end
